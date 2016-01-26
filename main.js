45baf2f60b6207f56fe0e886cedead964bf6a056
Clay John <clayjohn@shaw.ca>
2016-01-25 20:34:05 -0700

    Added a square sprite with gradient
    See summary.

--- main.js
+++ main.js
@@ -10,8 +10,24 @@ function create () {
     var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
 
     var text = game.add.text(game.world.centerX, game.world.centerY, "Succesful window open", style);
-
     text.anchor.set(0.5);
+    var bmd = game.add.bitmapData(128,128);
+
+    bmd.ctx.beginPath();
+
+    for(var i=0;i<128;i++) {
+  
+      for (var j=0;j<128;j++) {
+ 
+           bmd.setPixel(i, j, i*2, j*2, 255-(i+j));
+
+      }
+
+    }
+
+    bmd.ctx.fill();
+
+    var sprite = game.add.image(game.world.centerX, game.world.centerY, bmd);
 }
 
 function update() {
