28f71d58c26d552e9e4896d58dbcc34393c8c4e0
Caleb John <calebjohn4@gmail.com>
2016-02-04 20:55:35 -0700

    Add TravisCI to run JsHint and mocha tests
    Run jshint on all files

---
+++ test/test.js
@@ -0,0 +1,10 @@
+//Tests from mocha site, use until suitable tests are created for our project
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
