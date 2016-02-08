05a1fe60453006b2ec8e425719f0c16c71a40818
Clay John <clayjohn@shaw.ca>
2016-02-07 23:03:42 -0700

    fixed whitespace issues

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -21,16 +21,16 @@ Menu.prototype = {
   },
 
   create: function() {
-  	window.game.stage.backgroundColor = '#FFFFFF';
+    window.game.stage.backgroundColor = '#FFFFFF';
     window.game.scale.fullScreenScaleMode = Phaser.ScaleManager.NO_SCALE;
-  	//load Filter
-  	//this.initMenuBackground();
-  	//Apply filter to image in background
-  	this.backDrop = window.game.add.image();
-  	this.backDrop.width = window.game.width;
-  	this.backDrop.height = window.game.height;
+    //load Filter
+    //this.initMenuBackground();
+    //Apply filter to image in background
+    this.backDrop = window.game.add.image();
+    this.backDrop.width = window.game.width;
+    this.backDrop.height = window.game.height;
     this.backDropFilter = window.game.add.filter('sunset', window.game.width, window.game.height);
-  	this.backDrop.filters = [this.backDropFilter];
+    this.backDrop.filters = [this.backDropFilter];
     
     //draw title text
     this.titleText = window.game.add.text((window.game.width * 0.5 - 64) * 0.5, (window.game.height * 0.5 - 70) * 0.61, 'POTENTIAL FORTNIGHT');
@@ -38,45 +38,45 @@ Menu.prototype = {
     this.titleText.fontSize = 30;
     this.titleText.fill = 'white';
     this.titleText.alpha = 100;
-  	// make menu buttons
+    // make menu buttons
     this.drawMain();
   },
 
   update: function() {
-  	this.backDropFilter.update(window.game.input.activePointer);
+    this.backDropFilter.update(window.game.input.activePointer);
   },
 
   render: function() {
-  	
+    
   },
 
   drawMain: function() {
     //make buttons and initialize their functions
-  	this.startButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 20, 'START');
-  	this.startButton.events.onInputDown.add(function() {window.game.state.start('Cutscene');});
-  	this.optionButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 20, 'OPTIONS');
-  	this.optionButton.events.onInputDown.add(this.toggleMenu, this);
-  	this.fullscreenButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 60, 'FULLSCREEN');
-  	this.fullscreenButton.visible = false;
+    this.startButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 20, 'START');
+    this.startButton.events.onInputDown.add(function() {window.game.state.start('Cutscene');});
+    this.optionButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 20, 'OPTIONS');
+    this.optionButton.events.onInputDown.add(this.toggleMenu, this);
+    this.fullscreenButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 60, 'FULLSCREEN');
+    this.fullscreenButton.visible = false;
     this.fullscreenButton.events.onInputDown.add(this.fullscreenToggle, this);
-  	this.muteButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 20, 'SOUND: ' + (window.game.sound.mute ? 'OFF':'ON'));
-  	this.muteButton.visible = false;
-  	this.muteButton.events.onInputDown.add(function(target) {window.game.sound.mute = window.game.sound.mute === false; target.setText('SOUND: '+ (window.game.sound.mute ? 'OFF':'ON'));});
-  	this.seedButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 20, 'SEED: ');
-  	this.seedButton.visible = false;
-  	this.returnButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 60, 'RETURN');
-  	this.returnButton.visible = false;
-  	this.returnButton.events.onInputDown.add(this.toggleMenu, this);
+    this.muteButton = this.makeButton(window.game.world.centerX, window.game.world.centerY - 20, 'SOUND: ' + (window.game.sound.mute ? 'OFF':'ON'));
+    this.muteButton.visible = false;
+    this.muteButton.events.onInputDown.add(function(target) {window.game.sound.mute = window.game.sound.mute === false; target.setText('SOUND: '+ (window.game.sound.mute ? 'OFF':'ON'));});
+    this.seedButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 20, 'SEED: ');
+    this.seedButton.visible = false;
+    this.returnButton = this.makeButton(window.game.world.centerX, window.game.world.centerY + 60, 'RETURN');
+    this.returnButton.visible = false;
+    this.returnButton.events.onInputDown.add(this.toggleMenu, this);
   },
 
   //generic button creator, initializes buttons with standard formatting
   makeButton: function(x, y, name) {
-  	var button = window.game.add.text(x, y, name);
-  	button.anchor.setTo(0.5, 0.5);
-  	button.fontSize = 25;
-  	button.fill = 'white';
-  	button.inputEnabled = true;
-  	button.events.onInputOver.add(function(target) {target.alpha = 150;});
+    var button = window.game.add.text(x, y, name);
+    button.anchor.setTo(0.5, 0.5);
+    button.fontSize = 25;
+    button.fill = 'white';
+    button.inputEnabled = true;
+    button.events.onInputOver.add(function(target) {target.alpha = 150;});
     button.events.onInputOut.add(function(target) {target.alpha = 1;});
     return button;
   },
@@ -101,12 +101,12 @@ Menu.prototype = {
 
   //toggles between regular menu and options menu
   toggleMenu: function() {
-  	this.startButton.visible = this.startButton.visible === false;
-  	this.optionButton.visible = this.optionButton.visible === false;
-  	this.fullscreenButton.visible = this.fullscreenButton.visible === false;
-  	this.muteButton.visible = this.muteButton.visible === false;
-  	this.seedButton.visible = this.seedButton.visible === false;
-  	this.returnButton.visible = this.returnButton.visible === false;
+    this.startButton.visible = this.startButton.visible === false;
+    this.optionButton.visible = this.optionButton.visible === false;
+    this.fullscreenButton.visible = this.fullscreenButton.visible === false;
+    this.muteButton.visible = this.muteButton.visible === false;
+    this.seedButton.visible = this.seedButton.visible === false;
+    this.returnButton.visible = this.returnButton.visible === false;
   },
 
   // called in order to resize the game window, used with fullscreen
@@ -122,5 +122,5 @@ Menu.prototype = {
     this.muteButton.position.setTo(window.game.world.centerX, window.game.world.centerY - 20);
     this.seedButton.position.setTo(window.game.world.centerX, window.game.world.centerY + 20);
     this.returnButton.position.setTo(window.game.world.centerX, window.game.world.centerY + 60);
-  }   
+  }
 };
