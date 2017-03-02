4a44d66f9823c77153a67753e9d1e813713fd523
clayjohn <clayjohn@shaw.ca>
2017-03-02 01:41:02 -0700

    added functionality from phaser menu to godot menu

--- src/scripts/Main.gd
+++ src/scripts/Main.gd
@@ -1,7 +1,12 @@
 
 extends Control
 
+#right now this is in the root node of both Menu and Level
+#We may want to give each their own script in the future
+
 func _fixed_process(delta):
+	#Check if exit button is pressed
+	#this is configured in project settings
 	var escape = Input.is_action_pressed("exit")
 	if escape:
 		get_tree().quit()
--- src/scripts/MenuBackground.gd
+++ src/scripts/MenuBackground.gd
@@ -1,11 +1,23 @@
 
 extends Polygon2D
 
-func _fixed_process(delta):
-	var mat = get_material()
-	mat.set_shader_param("mouse", get_viewport().get_mouse_pos())
+var mat
+
+#updates size of polygon as well as the shader resolution uniform
+func update_resolution(fullscreen):
+	mat.set_shader_param("resolution", global.size)
+	var polygon = Vector2Array()
+	polygon.append(Vector2(0, 0))
+	polygon.append(Vector2(global.size.x, 0))
+	polygon.append(global.size)
+	polygon.append(Vector2(0, global.size.y))
+	set_polygon(polygon)
+	
+#updates sun position in shader
+func update_sun(pos):
+	mat.set_shader_param("mouse", pos)
 
 func _ready():
-	set_fixed_process(true)
+	mat = get_material()
 
 
