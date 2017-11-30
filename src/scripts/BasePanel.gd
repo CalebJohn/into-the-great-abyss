e965a86b1827a78eef611d614d22e501c1aa800d
clayjohn <clayjohn@shaw.ca>
2017-11-30 00:11:13 -0800

    setup framework for 3d scene generation

--- src/scripts/BasePanel.gd
+++ src/scripts/BasePanel.gd
@@ -4,3 +4,10 @@ signal switch_scene
 
 func _on_Button_pressed():
 	emit_signal("switch_scene")
+
+
+func _on_Scene_pressed():
+	if get_node("SceneView").is_visible():
+		get_node("Scene").set_text("Generate Scene")
+	else:
+		get_node("Scene").set_text("Hide Scene")
